9333a9489cc19efadba392ee3b8eb28dfe4605c5
Daniel Schep <dschep@gmail.com>
2015-12-28 15:13:03 -0500

    windows support & fix default notifier notify call

--- ntfy/backends/default.py
+++ ntfy/backends/default.py
@@ -3,8 +3,9 @@ from importlib import import_module
 
 
 def notify(subject, config, message=None, device=None):
-    for os in ['linux']:
+    for os in ['linux', 'win32']:
         if platform.startswith(os):
             module = import_module('ntfy.backends.{}'.format(os))
-            module.notify(subject, config, message, device)
+            module.notify(subject=subject, config=config,
+                          message=message, device=device)
             break
