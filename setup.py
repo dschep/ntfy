5376dba0d47fe126e71d29a907756d092355e2ee
Daniel Schep <dschep@gmail.com>
2016-01-04 17:10:16 -0500

    bump version for osx release!

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='ntfy',
 
-    version='0.1.3',
+    version='0.1.4',
 
     description='A utility for sending push notifications',
     long_description=long_description,
