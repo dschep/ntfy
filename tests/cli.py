e03cfce101406fdfb15d180d49ac96b64a9c1450
Daniel Schep <dschep@gmail.com>
2016-01-15 02:00:19 -0500

    fix mocking of open

--- tests/cli.py
+++ tests/cli.py
@@ -1,20 +1,20 @@
 from os import devnull
 from unittest import TestCase, main
 
-from mock import patch
+from mock import patch, mock_open
 
 from ntfy.cli import load_config, parser
 
 
 class TestLoadConfig(TestCase):
 
+    @mock_open()
     @patch('ntfy.cli.stderr')
-    @patch('ntfy.cli.open', side_effect=IOError())
     def test_default_config(self, *mocks):
         config = load_config(parser.parse_args(['-c', devnull, 'send', '']))
         self.assertEqual(config, {'backends': ['default']})
 
-    @patch('ntfy.cli.open')
+    @mock_open()
     @patch('ntfy.cli.json.load', lambda x: {'backend': 'foobar'})
     def test_backwards_compat(self, *mocks):
         config = load_config(parser.parse_args(['-c', devnull, 'send', '']))
