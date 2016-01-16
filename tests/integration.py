d097140401c2d4bd52e2bd5fa412172cd6ac06ff
Daniel Schep <dschep@gmail.com>
2016-01-15 19:46:40 -0500

    darwin test

--- tests/integration.py
+++ tests/integration.py
@@ -44,6 +44,28 @@ class TestIntegration(TestCase):
             if old_dbus is not None:
                 modules['dbus'] = old_dbus
 
+    @patch(('__builtin__' if py == 2 else 'builtins') +'.open', mock_open())
+    @patch('ntfy.cli.json.load')
+    def test_linux(self, mock_jsonload):
+        old_foundation = modules.get('Foundation')
+        old_objc = modules.get('objc')
+        old_appkit = modules.get('AppKit')
+        modules['Foundation'] = MagicMock()
+        modules['objc'] = MagicMock()
+        modules['AppKit'] = MagicMock()
+        try:
+            mock_jsonload.return_value = {
+                'backends': ['darwin'],
+            }
+            ntfy_main(['send', 'foobar'])
+        finally:
+            if old_foundation is not None:
+                modules['Foundation'] = old_foundation
+            if old_objc is not None:
+                modules['objc'] = old_objc
+            if old_appkit is not None:
+                modules['AppKit'] = old_appkit
+
 
 if __name__ == '__main__':
     main()
