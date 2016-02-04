f9f0d13026f91df34edf5acef702186a1a78d770
Daniel Schep <dschep@gmail.com>
2016-02-03 20:59:50 -0500

    oops, commited wrong file for pushover tests

--- tests/ntfy_test/pushover.py
+++ tests/ntfy_test/pushover.py
@@ -4,11 +4,23 @@ from mock import patch
 from ntfy.backends.pushover import notify
 
 
-def PushoverTestCase(TestCase):
+class TestPushover(TestCase):
     @patch('requests.post')
-    def test_success(self, mock_post):
-        notify('title', 'message')
-        mock_post.called_once_with()
+    def test_basic(self, mock_post):
+        notify('title', 'message', user_key='user_key')
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key', 'message': 'message', 'token':
+                  'aUnsraBiEZVsmrG89AZp47K3S2dX2a', 'title': 'title'})
+
+    @patch('requests.post')
+    def test_device(self, mock_post):
+        notify('title', 'message', user_key='user_key', device='foobar')
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key', 'message': 'message', 'token':
+                  'aUnsraBiEZVsmrG89AZp47K3S2dX2a', 'title': 'title',
+                  'device': 'foobar'})
 
 if __name__ == '__main__':
     main()
