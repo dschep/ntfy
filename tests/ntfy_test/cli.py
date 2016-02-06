3d89e82d88b400d928b69bd1ef56600b3ed76fde
Daniel Schep <dschep@gmail.com>
2016-02-06 08:51:56 -0500

    :shirt: cleaning up lint

--- tests/ntfy_test/cli.py
+++ tests/ntfy_test/cli.py
@@ -13,6 +13,7 @@ py = version_info.major
 mock_open_error = mock_open()
 mock_open_error.side_effect = IOError(ENOENT, 'foobar')
 
+
 class TestLoadConfig(TestCase):
 
     @patch(('__builtin__' if py == 2 else 'builtins') + '.open',
@@ -21,13 +22,14 @@ class TestLoadConfig(TestCase):
         config = load_config(parser.parse_args(['send', '']))
         self.assertEqual(config, {'backends': ['default']})
 
-    @patch(('__builtin__' if py == 2 else 'builtins') +'.open', mock_open())
+    @patch(('__builtin__' if py == 2 else 'builtins') + '.open', mock_open())
     @patch('ntfy.cli.yaml.load')
     def test_backwards_compat(self, mock_yamlload):
         mock_yamlload.return_value = {'backend': 'foobar'}
         config = load_config(parser.parse_args(['send', '']))
         self.assertIn('backends', config)
         self.assertEqual(config['backends'], ['foobar'])
+
 
 class TestTruthyish(TestCase):
     def test_truthyish(self):
@@ -53,6 +55,7 @@ class TestTruthyish(TestCase):
         self.assertFalse(truthyish('n'))
         self.assertFalse(truthyish('no'))
         self.assertFalse(truthyish('NO'))
+
 
 class TestRunCmd(TestCase):
     @patch('subprocess.call')
