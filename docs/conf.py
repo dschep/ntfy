365ac86c2dd9e2bdf6c4d5184a4ba3023f47464d
Daniel Schep <dschep@gmail.com>
2016-02-06 10:24:28 -0500

    :fork: me!

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,8 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'github_user': 'dschep', 'github_repo': 'ntfy',
+                      'github_banner': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
