120776fd6604a73825f396d86b067f12faff3841
Daniel Schep <dschep@gmail.com>
2016-02-19 20:28:53 -0500

    implement a --longer-than option for done. closes #23

--- ntfy/cli.py
+++ ntfy/cli.py
@@ -20,13 +20,16 @@ from .config import load_config, DEFAULT_CONFIG, OLD_DEFAULT_CONFIG
 def run_cmd(args):
     start_time = time()
     retcode = call(args.command)
+    duration = time() - start_time
+    if duration <= args.longer_than:
+        return
     if emojize is not None and not args.no_emoji:
         prefix = '\u2705 ' if retcode == 0 else '\u274C '
     else:
         prefix = ''
     return '{}"{}" {} in {:d}:{:02d} minutes'.format(
         prefix, ' '.join(args.command), 'succeeded' if retcode == 0 else
-        'failed', *map(int, divmod(time() - start_time, 60)))
+        'failed', *map(int, divmod(duration, 60)))
 
 
 parser = argparse.ArgumentParser(
@@ -95,6 +98,11 @@ done_parser = subparsers.add_parser(
 done_parser.add_argument('command',
                          nargs=argparse.REMAINDER,
                          help='command to run')
+done_parser.add_argument('-L',
+                         '--longer-than',
+                         type=int,
+                         metavar='N',
+                         help="Don't notify if command finishes ")
 done_parser.set_defaults(func=run_cmd)
 
 
@@ -140,8 +148,13 @@ def main(cli_args=None):
     if args.backend:
         config['backends'] = args.backend
 
+    if args.func == run_cmd and args.longer_than is None and 'longer_than' in config:
+        args.longer_than = config['longer_than']
+
     if hasattr(args, 'func'):
         message = args.func(args)
+        if message is None:
+            return 0
         if emojize is not None and not args.no_emoji:
             message = emojize(message, use_aliases=True)
         return notify(message, args.title, config, **dict(args.option))
