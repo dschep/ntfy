51a8b23954efc1045fdd157d6b569012603d43b2
Daniel Schep <dschep@endgame.com>
2016-03-24 00:48:39 -0400

    :white_check_mark: screw it no need to be fancy

--- tests/ntfy_test/pushover.py
+++ tests/ntfy_test/pushover.py
@@ -138,26 +138,42 @@ class TestPushover(TestCase):
                   'callback': 'http://example.com'},
             headers={'User-Agent': USER_AGENT})
 
+    @patch('requests.post')
+    def test_device(self, mock_post):
+        notify('title', 'message', user_key='user_key', device='foobar')
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                    'message': 'message',
+                    'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                    'title': 'title',
+                    'device': 'foobar'},
+            headers={'User-Agent': USER_AGENT})
 
-for option in ['device', 'sound', 'url']:
+    @patch('requests.post')
+    def test_sound(self, mock_post):
+        notify('title', 'message', user_key='user_key', sound='foobar')
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                    'message': 'message',
+                    'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                    'title': 'title',
+                    'sound': 'foobar'},
+            headers={'User-Agent': USER_AGENT})
 
-    def closure():
-        @patch('requests.post')
-        def test_option(self, mock_post):
-            option_kwarg = {option: 'foobar'}
-            notify('title', 'message', user_key='user_key', **option_kwarg)
-            mock_post.assert_called_once_with(
-                'https://api.pushover.net/1/messages.json',
-                data={'user': 'user_key',
-                      'message': 'message',
-                      'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
-                      'title': 'title',
-                      option: 'foobar'},
-                headers={'User-Agent': USER_AGENT})
+    @patch('requests.post')
+    def test_url(self, mock_post):
+        notify('title', 'message', user_key='user_key', url='foobar')
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                    'message': 'message',
+                    'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                    'title': 'title',
+                    'url': 'foobar'},
+            headers={'User-Agent': USER_AGENT})
 
-        setattr(TestPushover, 'test_{}'.format(option), test_option)
-
-    closure()
 
 if __name__ == '__main__':
     main()
