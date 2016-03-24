cd7f2b5cdab1b8d54eda15db91a6abbbaf3fae52
Daniel Schep <dschep@endgame.com>
2016-03-24 00:40:39 -0400

    :white_check_mark: improve pushover coverage

--- tests/ntfy_test/pushover.py
+++ tests/ntfy_test/pushover.py
@@ -34,6 +34,16 @@ class TestPushover(TestCase):
                   'url': 'bar'},
             headers={'User-Agent': USER_AGENT})
 
+        mock_post.reset_mock()
+        notify('title', 'message', user_key='user_key', url_title='foo')
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                  'message': 'message',
+                  'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                  'title': 'title'},
+            headers={'User-Agent': USER_AGENT})
+
     @patch('requests.post')
     def test_html(self, mock_post):
         notify('title', 'message', user_key='user_key', html=True)
@@ -56,6 +66,76 @@ class TestPushover(TestCase):
                   'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
                   'title': 'title',
                   'priority': 1},
+            headers={'User-Agent': USER_AGENT})
+
+    @patch('requests.post')
+    def test_invalid_priority(self, mock_post):
+        self.assertRaises(ValueError,
+                          notify,
+                          'title',
+                          'message',
+                          user_key='user_key',
+                          priority=3)
+
+    @patch('requests.post')
+    def test_hi_priority(self, mock_post):
+        notify('title', 'message', user_key='user_key', priority=2)
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                  'message': 'message',
+                  'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                  'title': 'title',
+                  'priority': 2,
+                  'retry': 30,
+                  'expire': 86400},
+            headers={'User-Agent': USER_AGENT})
+
+    @patch('requests.post')
+    def test_hi_priority_retry(self, mock_post):
+        notify('title', 'message', user_key='user_key', priority=2, retry=60)
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                  'message': 'message',
+                  'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                  'title': 'title',
+                  'priority': 2,
+                  'retry': 60,
+                  'expire': 86400},
+            headers={'User-Agent': USER_AGENT})
+
+    @patch('requests.post')
+    def test_hi_priority_expire(self, mock_post):
+        notify('title', 'message', user_key='user_key', priority=2, expire=60)
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                  'message': 'message',
+                  'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                  'title': 'title',
+                  'priority': 2,
+                  'retry': 30,
+                  'expire': 60},
+            headers={'User-Agent': USER_AGENT})
+
+    @patch('requests.post')
+    def test_hi_priority_callback(self, mock_post):
+        notify('title',
+               'message',
+               user_key='user_key',
+               priority=2,
+               callback='http://example.com')
+        mock_post.assert_called_once_with(
+            'https://api.pushover.net/1/messages.json',
+            data={'user': 'user_key',
+                  'message': 'message',
+                  'token': 'aUnsraBiEZVsmrG89AZp47K3S2dX2a',
+                  'title': 'title',
+                  'priority': 2,
+                  'retry': 30,
+                  'expire': 86400,
+                  'callback': 'http://example.com'},
             headers={'User-Agent': USER_AGENT})
 
 
