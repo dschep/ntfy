d3be1d244aef1757da65a731d629c19438b36f11
Daniel Schep <dschep@endgame.com>
2016-03-24 01:33:22 -0400

    :white_check_mark: more xmpp coverage

--- tests/ntfy_test/xmpp.py
+++ tests/ntfy_test/xmpp.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from mock import patch, MagicMock
 
-from ntfy.backends.xmpp import NtfySendMsgBot
+from ntfy.backends.xmpp import notify, NtfySendMsgBot
 from ntfy.config import USER_AGENT
 
 
@@ -23,3 +23,22 @@ class NtfySendMsgBotTestCase(TestCase):
         bot.start(MagicMock)
         mock_send_message.assert_called_with(mbody='message', msubject='title',
                                              mto='bar@foo')
+
+    @patch('sleekxmpp.ClientXMPP.send_presence')
+    @patch('sleekxmpp.ClientXMPP.get_roster')
+    @patch('sleekxmpp.ClientXMPP.disconnect')
+    @patch('sleekxmpp.ClientXMPP.send_message')
+    def test_start_mtype(self, mock_send_message, *other_mocks):
+        bot = NtfySendMsgBot('foo@bar', 'hunter2', 'bar@foo', 'title',
+                             'message', mtype='chat')
+        bot.start(MagicMock)
+        mock_send_message.assert_called_with(mbody='message', msubject='title',
+                                             mto='bar@foo', mtype='chat')
+
+class XMPPTestCase(TestCase):
+    @patch('os.path.isdir')
+    @patch('ntfy.backends.xmpp.NtfySendMsgBot')
+    def test_capath(self, mock_bot_class, mock_isdir):
+        notify('title', 'message', 'foo@bar', 'hunter2', 'bar@foo',
+               path_to_certs='/custom/ca')
+        self.assertEqual('/custom/ca', mock_bot_class().ca_certs)
