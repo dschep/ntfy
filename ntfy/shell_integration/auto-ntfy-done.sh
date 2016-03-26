36ab107e6d14146c27039c895b4e42ca52ae7b3a
Daniel Schep <dschep@gmail.com>
2016-03-26 16:28:08 -0400

    prefix globals to keep ntfy_pre* from conflicting with ntfy tab completing

--- ntfy/shell_integration/auto-ntfy-done.sh
+++ ntfy/shell_integration/auto-ntfy-done.sh
@@ -12,7 +12,7 @@ AUTO_NTFY_DONE_IGNORE=${AUTO_NTFY_DONE_IGNORE:-ntfy emacs info less mail man mel
 # force emoji example
 #AUTO_NTFY_DONE_EMOJI=true
 
-function ntfy_precmd () {
+function _ntfy_precmd () {
     [ -n "$ntfy_start_time" ] || return
     local duration=$(( $(date +%s) - $ntfy_start_time ))
     ntfy_start_time=''
@@ -31,21 +31,21 @@ function ntfy_precmd () {
     ntfy $AUTO_NTFY_DONE_OPTS -l ERROR send "$prefix\"$ntfy_command\" $human_retcode in $human_duration minutes"
 }
 
-function ntfy_preexec () {
+function _ntfy_preexec () {
     ntfy_start_time=$(date +%s)
     ntfy_command=$(echo "$1")
 }
 
-function contains_element() {
+function _contains_element() {
     local e
     for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
     return 1
 }
 
-if ! contains_element ntfy_preexec "${preexec_functions[@]}"; then
-    preexec_functions+=(ntfy_preexec)
+if ! _contains_element _ntfy_preexec "${preexec_functions[@]}"; then
+    preexec_functions+=(_ntfy_preexec)
 fi
 
-if ! contains_element ntfy_precmd "${precmd_functions[@]}"; then
-    precmd_functions+=(ntfy_precmd)
+if ! _contains_element _ntfy_precmd "${precmd_functions[@]}"; then
+    precmd_functions+=(_ntfy_precmd)
 fi
