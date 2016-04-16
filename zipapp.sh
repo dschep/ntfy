a639a84a7fea8cca7c7733b348925e701a9b0abe
Daniel Schep <dschep@gmail.com>
2016-04-16 11:16:44 -0400

    strip compiled and namespace crap from zipapp

--- zipapp.sh
+++ zipapp.sh
@@ -5,6 +5,9 @@ mkdir -p build/zipapp dist
 cp __main__.py build/zipapp
 pip install --target=build/zipapp -r requirements.txt .
 touch build/zipapp/ruamel/__init__.py  # ruamel is a namespace package
+find build/zipapp/ -name '*.pyc' -delete
+find build/zipapp/ -name '*.so' -delete
+find build/zipapp/ -name '*.pth' -delete
 python -m zipapp -h &> /dev/null && \
     python -m zipapp -o dist/ntfy --python /usr/bin/python build/zipapp || \
     echo 'zipapp only supported in Python3.5, zip build/zipapp yourself'
