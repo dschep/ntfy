25ba14fbc9f1c507a63f5789f2831446defb0954
Srinivas Devaki <mr.eightnoteight@gmail.com>
2016-08-30 11:38:29 +0530

    added notifico support, regarding #89

---
+++ tests/test_notifico.py
@@ -0,0 +1,45 @@
+from unittest import TestCase, main
+from mock import patch
+from requests import Response, HTTPError
+
+from ntfy.backends.notifico import notify
+
+
+class TestNotifico(TestCase):
+    def setUp(self):
+        self.webhook = 'https://n.tkte.ch/h/1234/testing_webhook'
+
+    @patch('requests.get')
+    def test_basic(self, mock_get):
+        resp = Response()
+        resp.status_code = 200
+        mock_get.return_value = resp
+        notify('title', 'message', webhook=self.webhook)
+        mock_get.assert_called_once_with(
+            self.webhook,
+            params={
+                'payload': 'title\nmessage'
+            })
+
+    @patch('requests.get')
+    def test_none_webhook(self, mock_get):
+        notify('title', 'message', webhook=None)
+        mock_get.assert_not_called()
+
+    @patch('requests.get')
+    def test_exception(self, mock_get):
+        resp = Response()
+        resp.status_code = 400
+        mock_get.return_value = resp
+        with self.assertRaises(HTTPError):
+            notify('title', 'message', webhook=self.webhook)
+        mock_get.assert_called_once_with(
+            self.webhook,
+            params={
+                'payload': 'title\nmessage'
+            })
+
+if __name__ == '__main__':
+    main()
+
+
