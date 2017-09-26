7a79b95cbc3c51c6fb9e214fccd25372717d0929
Daniel Schep <dschep@gmail.com>
2017-09-26 07:47:13 -0400

    version bump for updated bash-preexec & rocketchat

--- ntfy/__init__.py
+++ ntfy/__init__.py
@@ -6,7 +6,7 @@ from importlib import import_module
 from inspect import getargspec
 from .backends.default import DefaultNotifierError
 
-__version__ = '2.4.4'
+__version__ = '2.5.0'
 
 _user_home = path.expanduser('~')
 _cwd = getcwd()
