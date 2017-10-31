a63c9aeeced03a94de95cfbd4f93e1002e42e05f
Daniel Schep <dschep@gmail.com>
2017-10-31 12:50:26 -0400

    update bash-preexec

--- ntfy/shell_integration/bash-preexec.sh
+++ ntfy/shell_integration/bash-preexec.sh
@@ -11,7 +11,7 @@
 # Author: Ryan Caloras (ryan@bashhub.com)
 # Forked from Original Author: Glyph Lefkowitz
 #
-# V0.3.4
+# V0.3.6
 #
 
 # General Usage:
@@ -44,6 +44,9 @@ __bp_imported="defined"
 # functions, should they want it.
 __bp_last_ret_value="$?"
 __bp_last_argument_prev_command="$_"
+
+__bp_inside_precmd=0
+__bp_inside_preexec=0
 
 # Remove ignorespace and or replace ignoreboth from HISTCONTROL
 # so we can accurately invoke preexec with a command from our
@@ -83,9 +86,17 @@ __bp_interactive_mode() {
 # This function is installed as part of the PROMPT_COMMAND.
 # It will invoke any functions defined in the precmd_functions array.
 __bp_precmd_invoke_cmd() {
-
-    # Save the returned value from our last command
+    # Save the returned value from our last command. Note: this MUST be the
+    # first thing done in this function.
     __bp_last_ret_value="$?"
+
+    # Don't invoke precmds if we are inside an execution of an "original
+    # prompt command" by another precmd execution loop. This avoids infinite
+    # recursion.
+    if (( __bp_inside_precmd > 0 )); then
+      return
+    fi
+    local __bp_inside_precmd=1
 
     # Invoke every function defined in our function array.
     local precmd_function
@@ -136,6 +147,12 @@ __bp_preexec_invoke_exec() {
     # Save the contents of $_ so that it can be restored later on.
     # https://stackoverflow.com/questions/40944532/bash-preserve-in-a-debug-trap#40944702
     __bp_last_argument_prev_command="$1"
+
+    # Don't invoke preexecs if we are inside of another preexec.
+    if (( __bp_inside_preexec > 0 )); then
+      return
+    fi
+    local __bp_inside_preexec=1
 
     # Checks if the file descriptor is not standard out (i.e. '1')
     # __bp_delay_install checks if we're in test. Needed for bats to run.
@@ -281,7 +298,8 @@ __bp_install_after_session_init() {
     # trap. __bp_install sets PROMPT_COMMAND to its final value, so these are only
     # invoked once.
     # It's necessary to clear any existing DEBUG trap in order to set it from the install function.
-    PROMPT_COMMAND='__bp_trap_string=$(trap -p DEBUG); trap DEBUG; __bp_install'
+    # Using \n as it's the most universal delimiter of bash commands
+    PROMPT_COMMAND=$'\n__bp_trap_string="$(trap -p DEBUG)"\ntrap DEBUG\n__bp_install\n'
 }
 
 # Run our install so long as we're not delaying it.
