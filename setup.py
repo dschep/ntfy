80e99b01444ed32c2c8a6b30a50cce1cc5486151
Daniel Schep <dschep@gmail.com>
2017-12-29 18:09:21 -0500

    has been stable for a while already

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         'Environment :: Console',
 
