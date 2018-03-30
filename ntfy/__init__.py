2783160bd94768ab76c80d23aceefb2f6d4d0953
Daniel Schep <dschep@gmail.com>
2018-03-30 09:34:44 -0400

    version bump for 697f4a3

--- ntfy/__init__.py
+++ ntfy/__init__.py
@@ -6,7 +6,7 @@ from importlib import import_module
 from inspect import getargspec
 from .backends.default import DefaultNotifierError
 
-__version__ = '2.5.1'
+__version__ = '2.6.0'
 
 _user_home = path.expanduser('~')
 _cwd = getcwd()
