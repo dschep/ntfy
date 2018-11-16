aa6273bd4c4a4e40a861060c4fc757fe121e7866
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2018-11-16 16:27:41 +0100

    make strings unicode

--- ntfy/backends/matrix.py
+++ ntfy/backends/matrix.py
@@ -11,7 +11,7 @@ def notify(title, message, url, roomId, userId=None, token=None, password=None, 
         client.api.token = token
     else:
         raise Exception("Must supply 'token' or 'password'")
-    msg_plain = "**{}** {}".format(title, message)
-    msg_html = "<b>{}</b> {}".format(title, message)
+    msg_plain = u"**{}** {}".format(title, message)
+    msg_html = u"<b>{}</b> {}".format(title, message)
     room = client.join_room(roomId)
     room.send_html(msg_html, msg_plain)
