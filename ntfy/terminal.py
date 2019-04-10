8f51209286f21032c5a48dae98b304f5fb035d3d
Unai Fernandez <unai.ndz@gmail.com>
2019-04-10 22:22:00 +0200

    Fix linux_window_is_focused

--- ntfy/terminal.py
+++ ntfy/terminal.py
@@ -7,7 +7,7 @@ from sys import platform, stdout
 def linux_window_is_focused():
     xprop_cmd = shlex.split('xprop -root _NET_ACTIVE_WINDOW')
     try:
-        xprop_window_id = int(check_output(xprop_cmd, stdout=PIPE, stderr=PIPE).split()[-1], 16)
+        xprop_window_id = int(check_output(xprop_cmd, stderr=PIPE).split()[-1], 16)
     except CalledProcessError:
         return False
     except ValueError:
