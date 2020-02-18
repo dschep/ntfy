f38d06ec080c6b50601a5db94d6d636448bca161
Jonathan D. Backhaus <jonathan.david.backhaus@gmail.com>
2020-02-18 09:40:36 -0500

    Baseline test for webhook backend

---
+++ tests/test_webhook.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from mock import patch
+from ntfy.backends.webhook import notify
+from ntfy.config import USER_AGENT
+
+
+class TestWebhook(TestCase):
+    @patch('requests.post')
+    def test_basic(self, mock_post):
+        url = 'https://hooks.slack.com/services/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'
+        notify('title', 'message', url)
+        mock_post.assert_called_once_with(
+            url,
+            json={'title': 'title',
+                  'message': 'message'},
+            headers={'User-Agent': USER_AGENT})
