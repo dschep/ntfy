77d5d46bf2daabe3befe0c00837d59d18ec36a40
Backhaus, Jonathan D <jonathan.d.backhaus@lmco.com>
2020-02-19 11:06:45 -0500

    Add optional 'verify' argument to specify bundle (or disable cert. verification)

--- ntfy/backends/webhook.py
+++ ntfy/backends/webhook.py
@@ -2,10 +2,10 @@ import requests
 from ..config import USER_AGENT
 
 
-def notify(title, message, url, retcode=None):
+def notify(title, message, url, verify=True, retcode=None):
 
     payload = {"text": "*{}*\n{}".format(title, message)}
     headers = {'User-Agent': USER_AGENT}
 
-    response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=headers, verify=verify)
     response.raise_for_status()
