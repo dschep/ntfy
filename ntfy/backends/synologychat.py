d38a9892dc9e568ef894f003101ee0b6d9f9b1da
Cologler <skyoflw@gmail.com>
2020-10-06 23:51:25 +0800

    Create synologychat.py

---
+++ ntfy/backends/synologychat.py
@@ -0,0 +1,27 @@
+import json
+import warnings
+
+import requests
+import urllib3.exceptions
+
+def notify(title, message,
+           webhook_url, verify=True,
+           retcode=None):
+
+    body = 'payload=' + json.dumps({
+        'text': message
+    })
+
+    if bool(verify):
+        response = requests.post(webhook_url, body)
+    else:
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', urllib3.exceptions.InsecureRequestWarning)
+            response = requests.post(webhook_url, body, verify=False)
+
+    response.raise_for_status()
+
+    resp_body = response.json()
+    if not resp_body['success']:
+        print(response.text)
+        return 1
