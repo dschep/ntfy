1999dd18dbb2df877e2e012a24c5875bc633bf0e
JoshKing56 <joshuaiking56@gmail.com>
2021-08-19 16:19:22 +0100

    Added parse_mode help text

--- ntfy/backends/telegram.py
+++ ntfy/backends/telegram.py
@@ -8,11 +8,16 @@ config_dir = user_config_dir('ntfy', 'dschep')
 config_file = path.join(config_dir, 'telegram.ini')
 
 
-def notify(title, message, retcode=None, parse_mode="text"):
-    """Sends message over Telegram using telegram-send, title is ignored."""
+def notify(title, message, parse_mode="text", retcode=None):
+    """
+    Sends message over Telegram using telegram-send, title is ignored.
+
+    Optional parameters:
+    * ``parse_mode`` - How telegram parses the text. Possible options are "text", "markdown" or "html". More details here: https://core.telegram.org/bots/api#formatting-options
+    """
     if not path.exists(config_file):
         if not path.exists(config_dir):
             makedirs(config_dir)
         print("Follow the instructions to configure the Telegram backend.\n")
         configure(config_file)
-    send(messages=[message], conf=config_file, parse_mode)
+    send(messages=[message], parse_mode=parse_mode, conf=config_file)
