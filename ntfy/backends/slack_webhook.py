8bb778b4eb65a23f748fa6ffa697e1751bc9dbe0
Daniel Schep <dschep@gmail.com>
2021-10-18 10:35:29 -0400

    control more of the look of the slack webhook

--- ntfy/backends/slack_webhook.py
+++ ntfy/backends/slack_webhook.py
@@ -6,6 +6,8 @@ def notify(title, message, url, user, **kwargs):
     requests.post(
         url,
         json={
+            "username": "ntfy",
+            "icon_url": "https://ntfy.readthedocs.io/en/latest/_static/logo.png",
             "text": "{0}\n{1}".format(title, message),
             "channel": user,
             "blocks": [
