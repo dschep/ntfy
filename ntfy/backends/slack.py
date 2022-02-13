ae1f1f01af05c46e0bae685b85a6724ee733abc5
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-13 14:03:13 +0800

    Delete ntfy directory

--- ntfy/backends/slack.py
+++
@@ -1,8 +0,0 @@
-from slack_sdk import WebClient
-
-
-def notify(title, message, token, recipient, retcode=None):
-
-    slack = WebClient(token=token)
-
-    slack.chat_postMessage(channel=recipient, text=message)
