486ea7393f2ec0638f44a121fb3fd02a289f556b
Daniel Schep <dschep@gmail.com>
2025-03-05 08:51:05 -0500

    simpler

--- setup.py
+++ setup.py
@@ -47,10 +47,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='push notification',
